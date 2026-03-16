92c52dce2909af702f87bc4ceb0f6f4da23515ed
Esteban Leiva <130495376+Tebaah@users.noreply.github.com>
2026-03-16 15:37:05 +0000

    feat(01): se agregran operadores relacionales

---
+++ Retos/01/ejercio01/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// operadors aritmeticos
+
+var suma = 1 + 1;
+var resta = 5 - 10;
+var division = 25 / 5;
+var multiplicacion = 5 * 25;
+var modulo = 5 % 5;
+
+// operadores de asignacion
+
+// =
+// +=
+// -=
+// *=
+// /=
+
+// operadores relacionales
+
+var igualA = 5 == 5;
+var noIgual = 5 != 5;
+var mayorQue = 5 > 2;
+var menorQue = 10 < 25;
+var mayorOIgual = 10 >= 5;
+var menorOigual = 45 <= 34;
+
+// operador logicos
+
+
+
+
