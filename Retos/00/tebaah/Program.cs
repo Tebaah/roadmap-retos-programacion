31fc01ea62028e3910a28df7ca9289622f0ef93a
Esteban Leiva <130495376+Tebaah@users.noreply.github.com>
2026-03-16 14:23:11 +0000

    feat(00): reto 00 realizado

---
+++ Retos/00/tebaah/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// https://learn.microsoft.com/es-es/dotnet/csharp/
+
+// Comentario de linea
+
+/*
+Comentario
+de varias lineas
+*/
+
+// variable y constante
+int variable = 0;
+const float constante = 1.0f;
+
+// datos primitivos
+
+byte datoByte;
+short datoShort;
+int datoInt;
+long datoLong;
+
+float datoFloat;
+double  datoDouble;
+
+decimal datoDecimal;
+
+bool datoBool;
+
+char datoChar;
+string datoString;
+
+Console.WriteLine("Hola, C#");
